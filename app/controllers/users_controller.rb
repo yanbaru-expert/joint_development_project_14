57788412d6ad321ef9f1bb703cf58ec021464ff4
Seidai Inoue <shengdajingshang@gmail.com>
2020-05-12 15:23:18 +0900

    新規投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:title, :content)
+  end
 end
