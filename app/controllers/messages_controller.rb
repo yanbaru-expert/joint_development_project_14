bff728c381c646680e6adbe70d0c38e50393ed02
ume05 <yuu02out@gmail.com>
2020-05-14 00:23:08 +0900

    (messages)更新機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,6 +16,15 @@ class MessagesController < ApplicationController
 		@message = Message.find(params[:id])
 	end
 
+	def edit
+		@message = Message.find(params[:id])
+	end
+
+	def update
+		message = Message.find(params[:id])
+		message.update(message_params)
+	end
+
 	private
 
 	def message_params
